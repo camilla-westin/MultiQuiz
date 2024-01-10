397164529679fba960d6c879703f7cededc1317f
Camilla Westin <camilla@webstin.se>
2024-01-10 12:48:53 +0100

    Extracted card to separate component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script setup>
-import q from "./data/quizes.json"
-import {ref, watch} from "vue"
+import q from './data/quizes.json'
+import { ref, watch } from 'vue'
+import Card from './components/Card.vue'
 
 const quizes = ref(q)
-const search = ref("")
+const search = ref('')
 
 watch(search, () => {
- quizes.value = q.filter(quiz => quiz.name.toLowerCase().includes(search.value.toLowerCase()))
+  quizes.value = q.filter((quiz) => quiz.name.toLowerCase().includes(search.value.toLowerCase()))
 })
-
 </script>
 
 <template>
@@ -18,70 +18,40 @@ watch(search, () => {
       <input type="text" placeholder="Search..." v-model.trim="search" />
     </header>
     <div class="options-container">
-        <div v-for="quiz in quizes" :key="quiz.id" class="card">
-          <img :src="quiz.img" alt="Math quiz" />
-          <div class="card-text">
-            <h2>{{quiz.name}}</h2>
-            <p>{{quiz.questions.length}} questions</p>
-          </div>
-        </div>
+      <Card v-for="quiz in quizes" :key="quiz.id" :quiz="quiz" />
     </div>
   </div>
 </template>
 
 <style scoped>
-  .container {
-    max-width: 1000px;
-    margin: 0 auto;
-    padding: 10px;
-  }
+.container {
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 10px;
+}
 
-  header {
-    margin-bottom: 10px;
-    margin-top: 30px;
-    display: flex;
-    align-items: center;
-  }
+header {
+  margin-bottom: 10px;
+  margin-top: 30px;
+  display: flex;
+  align-items: center;
+}
 
-  header h1 {
-    font-weight: bold;
-    margin-right: 30px;
-  }
+header h1 {
+  font-weight: bold;
+  margin-right: 30px;
+}
 
-  header input {
-    border: none;
-    background-color: rgba(128,128,128,0.1);
-    padding: 10px;
-    border-radius: 5px;
-  }
+header input {
+  border: none;
+  background-color: rgba(128, 128, 128, 0.1);
+  padding: 10px;
+  border-radius: 5px;
+}
 
-  .options-container {
-    display: flex;
-    flex-wrap: wrap;
-    magin-top: 40px;
-  }
-
-  .card {
-    width: 310px;
-    overflow: hidden;
-    border-radius: 2%;
-    box-shadow: 1px 1px 10px rgba(0,0,0,0.1);
-    margin-bottom: 35px;
-    margin-right: 20px;
-    cursor: pointer;
-  }
-
-  .card img {
-    width: 100%;
-    height: auto;
-    margin: 0;
-  }
-
-  .card-text {
-    padding: 15px;
-  }
-
-  .card-text h2 {
-    font-weight: bold;
-  }
+.options-container {
+  display: flex;
+  flex-wrap: wrap;
+  magin-top: 40px;
+}
 </style>
